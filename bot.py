735ff803d71a33f96fd22f236f324aa118280d67
sytomas <sytomas@cisco.com>
2016-12-09 14:15:19 -0800

    updatedbot

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
-
+import itty
 from itty import *
 import urllib2
 import json
 import requests
-requests.packages.urllib3.disable_warnings()
+#requests.packages.urllib3.disable_warnings()
 from requests.auth import HTTPBasicAuth
 import base64
 import random
 from tinydb import TinyDB, Query
+from flask import Flask, request
+from ciscosparkapi import CiscoSparkAPI
+import os
+import sys
+import json
+import re
 
 def eventbriteorder():
       response = requests.get(
