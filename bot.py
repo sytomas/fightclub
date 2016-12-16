280134b32df401ba60c8fcc50b646394a6f76b86
sytomas <sytomas@cisco.com>
2016-12-16 15:46:52 -0800

    weather

--- bot.py
+++ bot.py
@@ -29,23 +29,23 @@ except ImportError:
 def wx():
     API_URL = "http://www.google.com/ig/api?"
     arguments = ArgumentParser(prog="weather")
-	unit = arguments.add_argument("--unit", choices="CF", dest="unit", default="C", help="Which unit to display the temperatures in")
-	location = arguments.add_argument("location", nargs="+")
-	args = arguments.parse_args(sys.argv[1:])
+    unit = arguments.add_argument("--unit", choices="CF", dest="unit", default="C", help="Which unit to display the temperatures in")
+    location = arguments.add_argument("location", nargs="+")
+    args = arguments.parse_args(sys.argv[1:])
 
-	for location in args.location:
-		url = API_URL + urlencode({"weather": location})
-		xml = urlopen(url).read()
-		doc = minidom.parseString(xml)
+    for location in args.location:
+        url = API_URL + urlencode({"weather": location})
+        xml = urlopen(url).read()
+        doc = minidom.parseString(xml)
 
-		forecast_information = doc.documentElement.getElementsByTagName("forecast_information")[0]
-		city = forecast_information.getElementsByTagName("city")[0].getAttribute("data")
+        forecast_information = doc.documentElement.getElementsByTagName("forecast_information")[0]
+        city = forecast_information.getElementsByTagName("city")[0].getAttribute("data")
 
-		current_conditions = doc.documentElement.getElementsByTagName("current_conditions")[0]
-		temp = current_conditions.getElementsByTagName("temp_f" if args.unit == "F" else "temp_c")[0].getAttribute("data")
-		condition = current_conditions.getElementsByTagName("condition")[0].getAttribute("data")
-		wind_condition = current_conditions.getElementsByTagName("wind_condition")[0].getAttribute("data")
-		humidity = current_conditions.getElementsByTagName("humidity")[0].getAttribute("data")
+        current_conditions = doc.documentElement.getElementsByTagName("current_conditions")[0]
+        temp = current_conditions.getElementsByTagName("temp_f" if args.unit == "F" else "temp_c")[0].getAttribute("data")
+        condition = current_conditions.getElementsByTagName("condition")[0].getAttribute("data")
+        wind_condition = current_conditions.getElementsByTagName("wind_condition")[0].getAttribute("data")
+        humidity = current_conditions.getElementsByTagName("humidity")[0].getAttribute("data")
     return(location, unit)
 # ****************** Get Weather ******************
 
