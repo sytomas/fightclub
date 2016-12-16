9b371b2dd677de94a6fbb80a6fbcf3f6bd969bdf
sytomas <sytomas@cisco.com>
2016-12-16 14:45:23 -0800

    updated

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from itty import *
-import urllib2, urllib
+import urllib2
 import json
 import requests
 requests.packages.urllib3.disable_warnings()
@@ -15,13 +15,6 @@ import os
 import sys
 import json
 import re
-from __future__ import print_function
-import httplib2
-from apiclient import discovery
-import oauth2client
-from oauth2client import client
-from oauth2client import tools
-
 
 def chucknorris():
     """
@@ -105,7 +98,7 @@ def index(request):
     print webhook['data']['id']
     result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
     result = json.loads(result)
-    msg = ''
+    msg = None
     if webhook['data']['personEmail'] != bot_email:
         in_message = result.get('text', '').lower()
         in_message = in_message.replace(bot_name, '')
@@ -143,8 +136,8 @@ def index(request):
                 msg += deviceserial[i]
                 msg += u'\n'
         elif 'help' in in_message:
-            msg = "/rules - Rules of Fight Club. \n"
-             "/fightgif - sends random Fight Club movie gifs. \n"
+            msg "/rules - Rules of Fight Club. \n"
+            print "/fightgif - sends random Fight Club movie gifs. \n"
             print "/chucknorris - no explanation needed."
         if msg != None:
             print msg
