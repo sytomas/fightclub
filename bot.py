d0ae863521e4a94ca315cf5211240e69f377cebf
sytomas <sytomas@cisco.com>
2016-12-30 15:53:35 -0800

    updated

--- bot.py
+++ bot.py
@@ -262,7 +262,7 @@ def chucknorris():
 
 def send_rules(post_data):
     for c in rules.items():
-        message = "* **%s**: %s \n" % (c[0], c[7])
+        message = "* **%s**: %s \n" % (c[0], c[6])
     return message
 
 #def rules():
