c0c6eb35b5fd7639a40ba6aeb19d35d191c1c87a
sytomas <sytomas@cisco.com>
2016-12-31 00:28:48 -0800

    updated

--- bot.py
+++ bot.py
@@ -247,11 +247,11 @@ def chucknorris():
     return joke
 
 def send_rules(post_data):
-    message = "Fight Club Rules: \n"
+    #message = "Fight Club Rules: \n"
     #for c in rules.items():
-    for c in rules:
-       #print(c)
-       message = message + str(c)
+    for c in rules.split('\n'):
+       message = str(c)
+       #message = message + str(c)
        #message = "* **%s**: %s \n" % (c[0], c[1])
     return message
 
